a776c5ef1f27e336a5b6bf9a4c9b64a5e8f19c41
hungnd1 <hungnd1@vivas.vn>
2016-03-03 15:18:01 +0700

    a

--- frontend/views/site/header.php
+++ frontend/views/site/header.php
@@ -1,5 +1,3 @@
-
-
 <!-- END SIDEBAR -->
 <!-- START PAGE-CONTAINER -->
 <div id="app" class="full-height">
@@ -7,75 +5,83 @@
         <!--START PAGE SIDEBAR-->
         <div class="page-sidebar" data-pages="sidebar" data-reactid=".0.0">
             <div id="appMenu" class="sidebar-overlay-slide from-top" data-reactid=".0.0.0"></div>
-            <div class="sidebar-header" data-reactid=".0.0.1">
+            <div class="sidebar-header" data-reactid=".0.0.1" style="padding-top: 10px;">
                 <a href="/" data-reactid=".0.0.1.0">
                     <img src="static/images/mm_sidebar.png" alt="Make Massive"
                          data-reactid=".0.0.1.0.0"></a></div>
             <div class="sidebar-menu" data-reactid=".0.0.3">
                 <ul class="menu-items" data-reactid=".0.0.3.0">
                     <li class="sidebar-user" data-reactid=".0.0.3.0.0">
-<!--                        <div class="user-left" data-reactid=".0.0.3.0.0.0">-->
-<!--                            <a style="cursor:pointer;" data-reactid=".0.0.3.0.0.0.0">-->
-<!--                                <img src="static/images/profile.png" alt=""-->
-<!--                                     class="user-photo img-circle" data-reactid=".0.0.3.0.0.0.0.0"></a></div>-->
+                        <!--                        <div class="user-left" data-reactid=".0.0.3.0.0.0">-->
+                        <!--                            <a style="cursor:pointer;" data-reactid=".0.0.3.0.0.0.0">-->
+                        <!--                                <img src="static/images/profile.png" alt=""-->
+                        <!--                                     class="user-photo img-circle" data-reactid=".0.0.3.0.0.0.0.0"></a></div>-->
                         <div class="user-right" data-reactid=".0.0.3.0.0.1">
                             <div data-reactid=".0.0.3.0.0.1.0">
-<!--                                <a class="user-widget-link" data-reactid=".0.0.3.0.0.1.0.0">-->
-<!--                                    <h5 style="cursor:pointer;display:inline-block;" data-reactid=".0.0.3.0.0.1.0.0.0">-->
-<!--                                        Login</h5></a>-->
-<!--                                <a class="m-l-20 user-widget-link" data-reactid=".0.0.3.0.0.1.0.1"><h5-->
-<!--                                        style="cursor:pointer;display:inline-block;" data-reactid=".0.0.3.0.0.1.0.1.0">-->
-<!--                                        Signup</h5></a></div>-->
-<!--                            <div data-reactid=".0.0.3.0.0.1.1">Guest</div>-->
-                        </div>
+                                <!--                                <a class="user-widget-link" data-reactid=".0.0.3.0.0.1.0.0">-->
+                                <!--                                    <h5 style="cursor:pointer;display:inline-block;" data-reactid=".0.0.3.0.0.1.0.0.0">-->
+                                <!--                                        Login</h5></a>-->
+                                <!--                                <a class="m-l-20 user-widget-link" data-reactid=".0.0.3.0.0.1.0.1"><h5-->
+                                <!--                                        style="cursor:pointer;display:inline-block;" data-reactid=".0.0.3.0.0.1.0.1.0">-->
+                                <!--                                        Signup</h5></a></div>-->
+                                <!--                            <div data-reactid=".0.0.3.0.0.1.1">Guest</div>-->
+                            </div>
                     </li>
-                    <li onclick="display('test',$(this));" class="m-t-10 active" data-reactid=".0.0.3.0.1"><span class="icon-thumbnail"
-                                                                              data-reactid=".0.0.3.0.1.0">
+                    <li onclick="display('test',$(this));" class="m-t-10 active" data-reactid=".0.0.3.0.1"><span
+                            class="icon-thumbnail"
+                            data-reactid=".0.0.3.0.1.0">
                     <i class="fa fa-money" data-reactid=".0.0.3.0.1.0.0"></i></span>
-                        <a  href="<?php echo \frontend\helpers\CUtils::createAbsoluteUrl(['site/get-list-has-offer']) ?>" data-reactid=".0.0.3.0.1.1"><span class="title" data-reactid=".0.0.3.0.1.1.0">Has Offer</span></a>
+                        <a href="<?php echo \frontend\helpers\CUtils::createAbsoluteUrl(['site/get-list-has-offer']) ?>"
+                           data-reactid=".0.0.3.0.1.1"><span class="title"
+                                                             data-reactid=".0.0.3.0.1.1.0">Has Offer</span></a>
                     </li>
-                    <li onclick="display('test1',$(this));" class="" data-reactid=".0.0.3.0.2"><span class="icon-thumbnail" data-reactid=".0.0.3.0.2.0"><i
+                    <li onclick="display('test1',$(this));" class="" data-reactid=".0.0.3.0.2"><span
+                            class="icon-thumbnail" data-reactid=".0.0.3.0.2.0"><i
                                 class="fa fa-file-code-o" data-reactid=".0.0.3.0.2.0.0"></i></span>
-                        <a href="<?php echo \frontend\helpers\CUtils::createAbsoluteUrl(['site/art-of-click']) ?>" data-reactid=".0.0.3.0.2.1"><span class="title"
-                                                                                       data-reactid=".0.0.3.0.2.1.0">ArtOfClick</span></a>
+                        <a href="<?php echo \frontend\helpers\CUtils::createAbsoluteUrl(['site/art-of-click']) ?>"
+                           data-reactid=".0.0.3.0.2.1"><span class="title"
+                                                             data-reactid=".0.0.3.0.2.1.0">ArtOfClick</span></a>
                     </li>
                     <li class="" data-reactid=".0.0.3.0.3"><span class="icon-thumbnail" data-reactid=".0.0.3.0.3.0"><i
                                 class="fa fa-group" data-reactid=".0.0.3.0.3.0.0"></i></span>
-                        <a href="<?php echo \frontend\helpers\CUtils::createAbsoluteUrl(['site/clicksmobs']) ?>" data-reactid=".0.0.3.0.3.1">
+                        <a href="<?php echo \frontend\helpers\CUtils::createAbsoluteUrl(['site/clicksmobs']) ?>"
+                           data-reactid=".0.0.3.0.3.1">
                             <span class="title" data-reactid=".0.0.3.0.3.1.0">ClickSmob</span></a>
                     </li>
                     <li class="" data-reactid=".0.0.3.0.4"><span class="icon-thumbnail" data-reactid=".0.0.3.0.4.0"><i
                                 class="fa fa-archive" data-reactid=".0.0.3.0.4.0.0"></i></span>
-                        <a href="<?php echo \frontend\helpers\CUtils::createAbsoluteUrl(['site/glispas']) ?>" data-reactid=".0.0.3.0.4.1">
+                        <a href="<?php echo \frontend\helpers\CUtils::createAbsoluteUrl(['site/glispas']) ?>"
+                           data-reactid=".0.0.3.0.4.1">
                             <span class="title" data-reactid=".0.0.3.0.4.1.0">GLISPA</span></a>
                     </li>
                     <li class="" data-reactid=".0.0.3.0.4"><span class="icon-thumbnail" data-reactid=".0.0.3.0.4.0"><i
                                 class="fa fa-cloud-upload" data-reactid=".0.0.3.0.4.0.0"></i></span>
-                        <a href="<?php echo \frontend\helpers\CUtils::createAbsoluteUrl(['site/error']) ?>" data-reactid=".0.0.3.0.4.1"><span
+                        <a href="<?php echo \frontend\helpers\CUtils::createAbsoluteUrl(['site/error']) ?>"
+                           data-reactid=".0.0.3.0.4.1"><span
                                 class="title" data-reactid=".0.0.3.0.4.1.0">Global Wide Media</span></a>
                     </li>
                     <li class="" data-reactid=".0.0.3.0.4"><span class="icon-thumbnail" data-reactid=".0.0.3.0.4.0"><i
                                 class="fa fa-desktop" data-reactid=".0.0.3.0.4.0.0"></i></span>
                         <a href="<?php echo \frontend\helpers\CUtils::createAbsoluteUrl(['site/matomies']) ?>"
-                                                                                                  data-reactid=".0.0.3.0.4.1"><span
+                           data-reactid=".0.0.3.0.4.1"><span
                                 class="title" data-reactid=".0.0.3.0.4.1.0">Matomy</span></a>
                     </li>
                     <li class="" data-reactid=".0.0.3.0.4"><span class="icon-thumbnail" data-reactid=".0.0.3.0.4.0"><i
                                 class="fa fa-eye" data-reactid=".0.0.3.0.4.0.0"></i></span>
                         <a href="<?php echo \frontend\helpers\CUtils::createAbsoluteUrl(['site/error']) ?>"
-                                                                                              data-reactid=".0.0.3.0.4.1"><span
+                           data-reactid=".0.0.3.0.4.1"><span
                                 class="title" data-reactid=".0.0.3.0.4.1.0">Mundo Media</span></a>
                     </li>
                     <li class="" data-reactid=".0.0.3.0.4"><span class="icon-thumbnail" data-reactid=".0.0.3.0.4.0"><i
                                 class="fa fa-cube" data-reactid=".0.0.3.0.4.0.0"></i></span>
                         <a href="<?php echo \frontend\helpers\CUtils::createAbsoluteUrl(['site/offer-seven']) ?>"
-                                                                                               data-reactid=".0.0.3.0.4.1"><span
+                           data-reactid=".0.0.3.0.4.1"><span
                                 class="title" data-reactid=".0.0.3.0.4.1.0">Offer Seven</span></a>
                     </li>
                     <li class="" data-reactid=".0.0.3.0.6"><span class="icon-thumbnail" data-reactid=".0.0.3.0.6.0"><i
                                 class="fa fa-cogs" data-reactid=".0.0.3.0.6.0.0"></i></span>
                         <a href="<?php echo \frontend\helpers\CUtils::createAbsoluteUrl(['site/setting']) ?>"
-                                                                                               data-reactid=".0.0.3.0.6.1"><span
+                           data-reactid=".0.0.3.0.6.1"><span
                                 class="title" data-reactid=".0.0.3.0.6.1.0">Setting</span></a>
                     </li>
                     <li class="" data-reactid=".0.0.3.0.5"><span class="icon-thumbnail" data-reactid=".0.0.3.0.5.0"><i
