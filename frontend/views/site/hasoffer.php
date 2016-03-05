622300a90ab177972611596b2c187d663a4ed04b
hungnd1 <hungnd1@vivas.vn>
2016-03-05 14:19:10 +0700

    a

--- frontend/views/site/hasoffer.php
+++ frontend/views/site/hasoffer.php
@@ -448,7 +448,7 @@
                 window.location.href = 'http://45.32.54.195/frontend/web/?r=site/get-list-offers&countries=' + x + '&device=' + device;
             }
             else if (typeof  check != 'undefined' && typeof  device == 'undefined') {
-                window.location.href = http://45.32.54.195/frontend/web/?r=site/get-list-offers&countries=' + x;
+                window.location.href = 'http://45.32.54.195/frontend/web/?r=site/get-list-offers&countries=' + x;
             } else if (typeof  check != 'undefined' && typeof  device != 'undefined' && typeof  network == 'undefined') {
                 window.location.href = 'http://45.32.54.195/frontend/web/?r=site/get-list-offers&countries=' + x + '&device=' + device;
             } else if (typeof  check != 'undefined' && typeof  device != 'undefined' && typeof  network != 'undefined') {
