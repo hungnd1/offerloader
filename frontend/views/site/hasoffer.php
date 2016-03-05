50d92f196109dbb59d4ed332268a7e714e7c4026
hungnd1 <hungnd1@vivas.vn>
2016-03-05 10:48:43 +0700

    a

--- frontend/views/site/hasoffer.php
+++ frontend/views/site/hasoffer.php
@@ -145,8 +145,9 @@
                                     <div style="display:inline-block;vertical-align:top;"
                                          data-reactid=".0.6.1.0.0.0.1.0.1.0:1.0.1">
                                         <div data-reactid=".0.6.1.0.0.0.1.0.1.0:1.0.1.1">
-                                            <a href="#" onclick="fnExcelReport1();" class="btn btn-primary">Export</a>
                                             <a href="#" onclick="Myfunction();" class="btn btn-primary">Filter</a>
+                                            <a href="#" onclick="fnExcelReport1();" class="btn btn-primary">Export</a>
+
                                         </div>
                                     </div>
                                 </div>
